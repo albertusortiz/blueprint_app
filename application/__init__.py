1f0a9fe729ffa5392d55f7241a29b0d899db79c5
Alberto Ortiz <alberto.ortiz.vargas@gmail.com>
2021-05-07 23:13:11 -0500

    Eliminando funcion run innecesaria

--- application/__init__.py
+++ application/__init__.py
@@ -3,11 +3,10 @@ from .inicio import inicio
 from .catalogo import catalogo
 from .contacto import contacto
 
-def run():
-    app = Flask(__name__)
-    app.config.from_pyfile('config/configuration.cfg')
-    app.register_blueprint(inicio)
-    app.register_blueprint(catalogo)
-    app.register_blueprint(contacto)
+app = Flask(__name__)
 
-    return app
+app.config.from_pyfile('config/configuration.cfg')
+
+app.register_blueprint(inicio)
+app.register_blueprint(catalogo)
+app.register_blueprint(contacto)
--- run.py
+++ run.py
@@ -1,5 +1,4 @@
-from application import run
+from application import app
 
 if __name__ == '__main__':
-    app = run()
     app.run()
